069f1342f2858e333fa0d64deff7e0231d247691
maciejb <maciejb@lsn.io>
2024-07-31 15:04:36 +0200

    pkd

---
+++ tests/src/tasks/pkdBlacklist/read/get-pkdBlackList.ts
@@ -0,0 +1,37 @@
+import { RestTask } from "@lsnova/supertest/dist/task/rest.task";
+import { expect } from "chai";
+import { AuthorizationSingleton } from "@lsnova/supertest/dist/authorization.singleton";
+import { TradeDev } from "../../constants";
+
+
+
+export class GetPkdBlacklist extends RestTask<any>{
+
+    name = 'get PKD blacklist'
+
+    constructor() {
+
+        super(TradeDev);
+    }
+
+    supports(holder: any): boolean {
+        return AuthorizationSingleton.isAuthorized()
+
+    }
+
+    beforeRequest(holder: any): void {
+
+    }
+    async process(holder: any): Promise<any> {
+        this.response = await this.http.get(`/api/trade-credits/secured/pkdBlacklist/list`, holder.queryParams).disableTLSCerts();
+
+    }
+    assertions(holder: any): void {
+        expect(this.response?.status).to.be.equal(200);
+    }
+
+    afterResponse(holder: any): void {
+
+    }
+}
+
